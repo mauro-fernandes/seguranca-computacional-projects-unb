e5f2ab7fa202df84d3ccc850fa679da57710827c
Mauro Fernandes de Almeida <maurofda@gmail.com>
2025-01-07 11:06:38 -0300

    Atualizar o main.py

--- app/main.py
+++ app/main.py
@@ -9,19 +9,12 @@ chave = "BEST"
 # Texto cifrado esperado: DCTXSWWVVVAMZ
 
 #########################################################################
-# Referências :::::::::::>
-# https://pages.mtu.edu/~shene/NSF-4/Tutorial/VIG/Vig-Examples.html
+# Referências https://pages.mtu.edu/~shene/NSF-4/Tutorial/VIG/Vig-Examples.html
 # https://www.geeksforgeeks.org/vigenere-cipher/
 # https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
 # https://intellipaat.com/blog/vigenere-cipher/
 #########################################################################
-#<<<<<<< patch-1
-#teste fork
-#=======
-#test 6may 12:31
-#test 6may 12:36
-#>>>>>>> main
-
+#
 
 if __name__ == '__main__':
     generated_key = generate_key_with_the_same_size_of_input(entrada, chave)
